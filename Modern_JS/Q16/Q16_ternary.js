b86310379691d063ff967560ec31389a9eed1912
Ajitha <ajitha.wipro@gmail.com>
2025-11-21 12:13:39 +0530

    new folder created

---
+++ Modern_JS/Q16/Q16_ternary.js
@@ -0,0 +1,8 @@
+const isEven=(n)=>{ return n%2==0?true:false}
+console.log(isEven(6));
+
+
+marks>=35?"Pass":"Fail";
+
+greet=(name)=>{return name!=null?"Hello,"+ name:"Hello, Guest"}
+console.log(greet("Alice"));
