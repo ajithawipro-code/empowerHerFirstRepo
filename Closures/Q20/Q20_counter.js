d5118a7d733713faf1b9eb5633b98d4c75de6a07
Ajitha <ajitha.wipro@gmail.com>
2025-11-24 21:56:41 +0530

    fullcode

--- Closures/Q20/Q20_counter.js
+++ Closures/Q20/Q20_counter.js
@@ -26,3 +26,42 @@ createcounter.increment();
 createcounter.increment();
 createcounter.increment();
 
+
+
+function createBankAccount()
+{
+    let balance;
+    let amount;
+    function deposit(bal)
+    {
+        balance=bal;
+        console.log("Deposited",balance);
+    }
+    function withdraw(amt)
+    {
+        if(balance>amt)
+        {
+            amount=amt;
+            balance=balance-amount;
+            console.log("Withdrawn",amount);
+            console.log("Balance here is-->",balance);
+        }
+        else{
+            console.log("Insufficient balance");
+        }
+    }
+    function curr_bal()
+    {
+         console.log("Available balance",balance);
+    }
+    return {deposit,withdraw,curr_bal};
+
+}
+
+const account=createBankAccount();
+account.deposit(500);
+account.withdraw(200);
+account.withdraw(50);
+account.curr_bal();
+console.log(account.balance); //undefined
+
